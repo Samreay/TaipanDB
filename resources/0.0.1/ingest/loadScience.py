aa6151e6a0558ede923aeb1fd363302abf0eadc6
= <marc.white@anu.edu.au>
2016-03-31 15:42:34 +1100

    Correctly populated loadScience.py

--- resources/0.0.1/ingest/loadScience.py
+++ resources/0.0.1/ingest/loadScience.py
@@ -6,7 +6,7 @@ from scripts.create import insert_into
 def execute(cursor, science_file=None):
     logging.info("Loading Science")
 
-    if not guides_file:
+    if not science_file:
         logging.info("No file passed - aborting loading science")
         return
 
@@ -15,12 +15,13 @@ def execute(cursor, science_file=None):
 
     # Do some stuff to convert science_table into values_table
     # (This is dependent on the structure of science_file)
-    values_table1 = [[row['uniqid'], row['ra_1'], row['dec_1'],
+    values_table1 = [[row['uniqid'], row['ra'], row['dec'],
                       True, False, False]
                      for row in science_table]
     columns1 = ["TARGET_ID", "RA", "DEC", "IS_SCIENCE", "IS_STANDARD",
                 "IS_GUIDE"]
-    values_table2 = [[row['uniqid'], False, False, False]
+    values_table2 = [[row['uniqid'],
+                      row['is_H0'], row['is_vpec'], row['is_lowz']]
                      for row in science_table]
     columns2 = ["TARGET_ID", "IS_H0", "IS_VPEC", "IS_LOWZ"]
 
@@ -28,5 +29,6 @@ def execute(cursor, science_file=None):
     if cursor is not None:
         insert_into(cursor, "target", values_table1, columns=columns1)
         insert_into(cursor, "science_target", values_table2, columns=columns2)
-
-    logging.info("Loaded Science")
+        logging.info("Loaded Science")
+    else:
+        logging.info("No database - however, dry-run of loading successful")
