696fc6aa8618be3ccb5be129fc12d9a8aef43689
= <marc.white@anu.edu.au>
2016-05-12 13:13:44 +1000

    Fix to str_pqsl to stop in turning values lists into single-quoted strings

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -3,7 +3,7 @@
 
 def str_psql(x):
     if isinstance(x, str):
-        if 'VALUES' not in x or x[0] == '(':
+        if 'VALUES' not in x and x[0] != '(':
             return "'%s'" % (x,)
     return str(x)
 
