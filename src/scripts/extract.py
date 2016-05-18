7748343b455f0216fc0933b9af13d289291105bb
= <marc.white@anu.edu.au>
2016-05-18 13:13:00 +1000

    Attempting to rectify issues with reading Null data back from database into numpy structured arrays

--- src/scripts/extract.py
+++ src/scripts/extract.py
@@ -126,6 +126,7 @@ def extract_from(cursor, table, conditions=None, columns=None):
     result = np.asarray(result, dtype={
         "names": columns,
         "formats": [psql_to_numpy_dtype(dtype) for dtype in dtypes],
+        "titles": columns,
         })
 
     return result
