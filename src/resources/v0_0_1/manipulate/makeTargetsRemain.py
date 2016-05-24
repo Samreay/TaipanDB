b6067cd88a3f81a75228077cee38637803629d89
= <marc.white@anu.edu.au>
2016-05-24 13:21:17 +1000

    Improved logging for makeTargetsRemain

--- src/resources/v0_0_1/manipulate/makeTargetsRemain.py
+++ src/resources/v0_0_1/manipulate/makeTargetsRemain.py
@@ -13,12 +13,14 @@ def execute(cursor):
     logging.info('Bulk-computing sci targets observed/remaining for tiles')
 
     # Read-in tiles which do not have n_sci_rem and n_sci_obs defined
+    logging.debug('Extracting tiles')
     tile_db = extract_from_joined(cursor, ['field', 'tile', 'tiling_info'],
                                   conditions=[('n_sci_rem', 'IS', 'NULL'),
                                               ('n_sci_obs', 'IS', 'NULL')],
                                   columns=['field_id', 'tile_pk', 'ra', 'dec'])
     # Cut this list down so there is only one tile per field (given this
     # is really a per-field calculation)
+    logging.debug('Constructing field tiles')
     fields = list(set([t['field_id'] for t in tile_db]))
     field_db = [(t for t in tile_db if t['field_id'] == field).next() for
                 field in fields]
@@ -29,6 +31,7 @@ def execute(cursor):
 
     # Read in all science targets in two lists - those which are 'done', and
     # those which are not
+    logging.debug('Extracting science targets from DB')
     sci_rem = extract_from_joined(cursor, ['target', 'science_target'],
                                   conditions=[('done', '=', False)],
                                   columns=['target_id', 'ra', 'dec',
@@ -45,6 +48,7 @@ def execute(cursor):
 
     # For each field number, compute the n_sci_obs and n_sci_rem (store in a
     # dict indexed by field ID)
+    logging.debug('Generating data to write back to DB')
     n_sci_obs = {}
     n_sci_rem = {}
     for tile in tile_list:
@@ -59,6 +63,11 @@ def execute(cursor):
                    n_sci_rem[tile['field_id']]] for tile in tile_db]
 
     # Write the data back
+    logging.debug('Write to DB')
     update_rows(cursor, 'tiling_info', write_back,
                 columns=['tile_pk', 'n_sci_obs', 'n_sci_rem'])
+
+    logging.info('Generation of sci targets obs/remaining complete!')
+
     return
+
