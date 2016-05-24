9a951e2303201f5fbeaf04323f77597d27f38b90
= <marc.white@anu.edu.au>
2016-05-24 12:23:07 +1000

    First pass at makeTilesRemain.py

--- src/resources/v0_0_1/manipulate/makeTargetsRemain.py
+++ src/resources/v0_0_1/manipulate/makeTargetsRemain.py
@@ -1,13 +1,64 @@
 # Compute the targets observed & remaining in individual tiles/fields
+# This script/function is designed for bulk insertions into tiling_info;
+# for smaller-scale updates, other functions will be required
 
-# Read-in tiles which do not have n_sci_rem and n_sci_obs defined
+import logging
 
-# Read in all science targets in two lists - those which are 'done', and
-# those which are not
+from src.scripts.extract import extract_from_joined
+from src.scripts.manipulate import update_rows
+from taipan.core import TaipanTarget, TaipanTile
 
-# For each field number, compute the n_sci_obs and n_sci_rem (store in a
-# dict indexed by field ID)
 
-# Construct a list to write back on a per-file basis
+def execute(cursor):
+    logging.info('Bulk-computing sci targets observed/remaining for tiles')
 
-# Write the data back
+    # Read-in tiles which do not have n_sci_rem and n_sci_obs defined
+    tile_db = extract_from_joined(cursor, ['field', 'tile', 'tiling_info'],
+                                  conditions=[('n_sci_rem', 'IS', 'NULL'),
+                                              ('n_sci_obs', 'IS', 'NULL')],
+                                  columns=['field_id', 'tile_pk', 'ra', 'dec'])
+    # Cut this list down so there is only one tile per field (given this
+    # is really a per-field calculation)
+    fields = list(set([t['field_id'] for t in tile_db]))
+    field_db = [(t for t in tile_db if t['field_id'] == field).next() for
+                field in fields]
+    tile_list = [TaipanTile(x['ra'],
+                            x['dec'],
+                            field=x['field_id']) for
+                 x in field_db]
+
+    # Read in all science targets in two lists - those which are 'done', and
+    # those which are not
+    sci_rem = extract_from_joined(cursor, ['target', 'science_target'],
+                                  conditions=[('done', '=', False)],
+                                  columns=['target_id', 'ra', 'dec',
+                                           'ux', 'uy', 'uz'])
+    sci_rem_targets = [TaipanTarget(x['target_id'], x['ra'], x['dec'],
+                                    ucposn=(x['ux'], x['uy'], x['uz'])) for
+                       x in sci_rem]
+    sci_obs = extract_from_joined(cursor, ['target', 'science_target'],
+                                  conditions=[('done', '=', True)],
+                                  columns=['target_id', 'ra', 'dec'])
+    sci_obs_targets = [TaipanTarget(x['target_id'], x['ra'], x['dec'],
+                                    ucposn=(x['ux'], x['uy'], x['uz'])) for
+                       x in sci_obs]
+
+    # For each field number, compute the n_sci_obs and n_sci_rem (store in a
+    # dict indexed by field ID)
+    n_sci_obs = {}
+    n_sci_rem = {}
+    for tile in tile_list:
+        n_sci_obs[tile['field_id']] = len(tile.available_targets(
+            sci_obs_targets))
+        n_sci_rem[tile['field_id']] = len(tile.available_targets(
+            sci_rem_targets))
+
+    # Construct a list to write back on a per-tile basis
+    write_back = [[tile['tile_pk'],
+                   n_sci_obs[tile['field_id']],
+                   n_sci_rem[tile['field_id']]] for tile in tile_db]
+
+    # Write the data back
+    update_rows(cursor, 'tiling_info', write_back,
+                columns=['tile_pk', 'n_sci_obs', 'n_sci_rem'])
+    return
