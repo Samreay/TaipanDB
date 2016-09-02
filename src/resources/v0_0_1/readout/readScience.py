1d65ce484fec1e9f174a07d5dface329d79400a8
= <marc.white@anu.edu.au>
2016-09-02 11:39:42 +1000

    Altered readScience to accept an 'unassigned' option

--- src/resources/v0_0_1/readout/readScience.py
+++ src/resources/v0_0_1/readout/readScience.py
@@ -1,11 +1,11 @@
 import logging
 import sys
 import os
-from ....scripts.extract import extract_from_joined
+from ....scripts.extract import extract_from_joined, extract_from_left_joined
 from taipan.core import TaipanTarget
 
 
-def execute(cursor, unobserved=False, target_ids=None):
+def execute(cursor, unobserved=False, unassigned=False, target_ids=None):
     """
     Extract science targets from the database
 
@@ -16,6 +16,9 @@ def execute(cursor, unobserved=False, target_ids=None):
     unobserved:
         Optional; Boolean value denoting whether to only return targets
         not marked as done (True) or all targets (False). Defaults to False.
+    unassigned:
+        Optional; Boolean value denoting whether to only return targets
+        not marked as assigned (True) or all targets (False). Defaults to False.
     target_ids:
         Optional; list of target_ids corresponding to the targets to extract
         from the database. Defaults to None, at which point all targets present
@@ -32,16 +35,30 @@ def execute(cursor, unobserved=False, target_ids=None):
 
     if unobserved:
         conditions += [('done', 'IS', False)]
+    if unassigned:
+        conditions += [('bug_id', 'IS', 'NULL'),
+                       ('is_observed', '=', False)]
     if target_ids is not None:
         conditions += [('target_id', 'IN', target_ids)]
 
-    targets_db = extract_from_joined(cursor, ['target', 'science_target'],
-                                     conditions=conditions + [
-                                         ('is_science', "=", True, )
-                                     ],
-                                     columns=['target_id', 'ra', 'dec',
-                                              'ux', 'uy', 'uz', 'priority',
-                                              'difficulty'])
+    # Old query (no ability to do unassigned)
+    # targets_db = extract_from_joined(cursor, ['target', 'science_target'],
+    #                                  conditions=conditions + [
+    #                                      ('is_science', "=", True, )
+    #                                  ],
+    #                                  columns=['target_id', 'ra', 'dec',
+    #                                           'ux', 'uy', 'uz', 'priority',
+    #                                           'difficulty'])
+
+    targets_db = extract_from_left_joined(cursor, ['target', 'science_target',
+                                                   'target_field', 'tile'],
+                                          conditions=conditions + [
+                                              ('is_science', "=", True,)
+                                          ],
+                                          columns=['target_id', 'ra', 'dec',
+                                                   'ux', 'uy', 'uz', 'priority',
+                                                   'difficulty'],
+                                          distinct=True)
 
     return_objects = [TaipanTarget(
         g['target_id'], g['ra'], g['dec'], priority=g['priority'],
