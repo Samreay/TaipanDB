968d63f0d49a5cbfecfa4a63e3cd6fc0524e36b2
= <marc.white@anu.edu.au>
2016-09-20 15:59:21 +1000

    Added insertAlmanac function

--- src/resources/v0_0_1/insert/insertAlmanac.py
+++ src/resources/v0_0_1/insert/insertAlmanac.py
@@ -2,7 +2,9 @@
 
 import logging
 from taipan.core import TaipanTile
+from taipan.scheduling import DarkAlmanac
 from ....scripts.create import insert_many_rows
+from ....scripts.manipulate import upsert_many_rows
 from ....scripts.extract import extract_from, extract_from_joined
 
 import datetime
@@ -28,7 +30,7 @@ from ..readout import readScience as rSc
 #     query_result = rSc.execute(cursor)
 
 
-def execute(cursor, field_id, almanac_data):
+def execute(cursor, field_id, almanac, dark_almanac=None, update=False):
     """
     Insert the given tiles into the database.
 
@@ -39,14 +41,20 @@ def execute(cursor, field_id, almanac_data):
     field_id:
         The field_id for the almanac to be written. This field must already
         exist in the field table.
-    almanac_data:
-        The almanac data to write to disk. This should be a list of 3-tuples,
-        where each tuple takes the form
-        (datetime, airmass, sun_alt, is_dark)
-        with data types
-        (datetime.datetime, float, float, boolean)
-        respectively.
-
+    almanac:
+        The taipan.scheduling.Almanac object to write to the database.
+    dark_almanac:
+        Optional; the dark_almanac corresponding to the almanac passed. Needed
+        to put in sun_alt, is_dark etc. values. Defaults to None, at which
+        point a new corresponding DarkAlmanac will be generated. Note that a
+        fresh DarkAlmanac will also be generated if the passed DarkAlmanac does
+        not exactly match the almanac in terms of start_date, end_date and
+        resolution.
+    update:
+        Optional; Boolean value denoting whether to update any almanac data
+        points already existing in the database (True) or simply not write
+        new values to those points (False). Defaults to False. Using True should
+        only be necessary if, e.g. fields have changed.
 
     Returns
     -------
@@ -54,5 +62,37 @@ def execute(cursor, field_id, almanac_data):
     """
     logging.info('Inserting almanacs into database...')
 
+    # Input checking
+    gen_dark_alm = False
+    if dark_almanac is not None:
+        if dark_almanac.start_date != almanac.start_date or \
+                        dark_almanac.end_date != almanac.end_date or \
+                        dark_almanac.resolution != almanac.resolution:
+            gen_dark_alm = True
+    else:
+        gen_dark_alm = True
+    if gen_dark_alm:
+        dark_almanac = DarkAlmanac(almanac.start_date,
+                                   end_date=almanac.end_date,
+                                   resolution=almanac.end_date)
+
+    # Format the almanac data into something that can inserted into the database
+    data_out = [(field_id, dt, almanac.airmass[dt], dark_almanac.sun_alt[dt],
+                 dark_almanac.dark_time[dt]) for dt in almanac.airmass.keys()]
+
+    # Write the data to the db
+    if update:
+        upsert_many_rows(cursor, 'observability',
+                         data_out,
+                         columns=['field_id', 'date', 'airmass',
+                                  'sun_alt', 'dark'])
+    else:
+        insert_many_rows(cursor, 'observability',
+                         data_out,
+                         columns=['field_id', 'date', 'airmass',
+                                  'sun_alt', 'dark'],
+                         skip_on_conflict=True)
+
+    logging.info('Insertion of almanac completed')
 
     return
