5838a4545f93aba3fadceb68a7fcfa07f82f3abe
= <marc.white@anu.edu.au>
2017-03-16 14:41:06 +1100

    Fix to manipulate

--- src/scripts/manipulate.py
+++ src/scripts/manipulate.py
@@ -149,18 +149,20 @@ def update_rows(cursor, table, data, columns=None,
                             for i in range(columns_to_match)])
 
     if conditions:
-        for condition in conditions:
-            if len(condition) == 3:
-                condition = ('t.' + condition[0], condition[1], condition[2], )
-            elif len(condition) == 5:
-                condition = (condition[0],
-                             't.' + condition[1],
-                             condition[2],
-                             condition[3],
-                             condition[4])
+        for i in range(len(conditions)):
+            if len(conditions[i]) == 3:
+                conditions[i] = ('t.' + conditions[i][0],
+                                 conditions[i][1],
+                                 conditions[i][2], )
+            elif len(conditions[i]) == 5:
+                conditions[i] = (conditions[i][0],
+                                 't.' + conditions[i][1],
+                                 conditions[i][2],
+                                 conditions[i][3],
+                                 conditions[i][4])
             else:
                 raise ValueError('condition %s is not a correct length' %
-                                 str(condition))
+                                 str(conditions[i]))
         conditions_string = generate_conditions_string(conditions)
         # Note we use AND here, because WHERE has already been invoked above
         string += ' AND %s' % conditions_string
