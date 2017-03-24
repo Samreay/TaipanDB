8eb6045d975be0b3eb564ebacc04e8d439ceea5b
= <marc.white@anu.edu.au>
2017-03-24 19:45:35 +1100

    Added makeDBReset

---
+++ src/resources/v0_0_1/manipulate/makeDBReset.py
@@ -0,0 +1,32 @@
+# This is a utility script, designed to reset the database to its
+# pre-simulation state
+# Note that if database structures have changed, or *any* target/field
+# catalogues have been updated, a standard database rebuild will
+# be required
+
+import logging
+
+from taipan.simulate.utils.updatesci import update_science_targets
+
+
+def execute(cursor):
+    # Remove the tiles
+    logging.info('Deleting tiles...')
+    cursor.execute('DELETE FROM tile')
+    logging.info('Done!')
+    # Reset the target visits/repeats/observations, and undo any
+    # done/success information
+    logging.info('Resetting target information...')
+    cursor.execute('UPDATE science_target SET '
+                   'observations=0,'
+                   'visits=0,'
+                   'repeats=0,'
+                   'done=NULL,'
+                   'success=False')
+    logging.info('Done!')
+    # Re-compute the target priority and difficulty information
+    logging.info('Updating science target information...')
+    update_science_targets(cursor, do_tp=True, do_d=True)
+    logging.info('Done!')
+
+    return
