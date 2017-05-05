17220b241699bf9358b367397953b5ffe0e505cd
= <marc.white@anu.edu.au>
2017-05-05 10:13:41 +1000

    Let mScVI increment visits by an arbitraty number

--- src/resources/v0_0_1/manipulate/makeScienceVisitInc.py
+++ src/resources/v0_0_1/manipulate/makeScienceVisitInc.py
@@ -5,7 +5,7 @@ import logging
 from ....scripts.manipulate import increment_rows
 
 
-def execute(cursor, target_ids):
+def execute(cursor, target_ids, inc=1):
     """
     Increment the visit number of the passed targets
 
@@ -15,6 +15,8 @@ def execute(cursor, target_ids):
         psycopg2 cursor for communicating with the database.
     target_ids:
         The list of target IDs to update the visit number for.
+    inc:
+        The increment value. Defaults to 1.
 
     Returns
     -------
@@ -30,9 +32,9 @@ def execute(cursor, target_ids):
     # Increment the rows
     if len(target_ids) > 0:
         increment_rows(cursor, 'science_target', 'visits',
-                       ref_column='target_id', ref_values=target_ids, inc=1)
+                       ref_column='target_id', ref_values=target_ids, inc=inc)
         # Increment the observations value of the rows
         increment_rows(cursor, 'science_target', 'observations',
-                       ref_column='target_id', ref_values=target_ids, inc=1)
+                       ref_column='target_id', ref_values=target_ids, inc=inc)
 
     return
