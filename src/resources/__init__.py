42de8e178f2be23dedd09fd2414a57d4bbbed918
= <marc.white@anu.edu.au>
2017-10-24 16:33:29 +1100

    Added symbolic links for tracking of current/dev versions

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,8 +1,10 @@
 """
-0.0.1 - Initial simulator work.
+``resources`` contains the various versions of the core TaipanDB code. The
+current stable version (``stable``) and current development version (``dev``)
+are maintained using symlinks, to allow multiple distributions of the code.
 
-Currently, this is the only version submodule within
-:any:`src.resources`. We are working on how to handle adding additional
-versions to this module in a robust way (i.e. doesn't require bulk rewrites of
-code which depends on it).
+Importing functions from TaipanDB should be done referring to either ``stable``
+or ``dev``, rather than importing directly from a numbered version. Within each
+version, the code should only import from that numbered version for internal
+consistency.
 """
