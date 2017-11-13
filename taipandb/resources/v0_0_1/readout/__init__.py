0d13351828c8df7907195590360b7052efd499f4
= <marc.white@anu.edu.au>
2017-11-13 15:53:18 +1100

    Fixed obs def filename regex

--- taipandb/resources/v0_0_1/readout/__init__.py
+++ taipandb/resources/v0_0_1/readout/__init__.py
@@ -13,7 +13,7 @@ from taipan.core import JSON_DTFORMAT_NAIVE
 # If you change one of these, make sure you update all the others to match!
 # Observing DT, tile PK, field ID
 OBS_DEF_FILENAME = '%s_tile%07d_field%05d_config.json'
-OBS_DEF_FILENAME_REGEX = re.compile(r'^*?(?P<dt>[0-9]{4}-[0-9]{2}-[0-9]{2}T'
+OBS_DEF_FILENAME_REGEX = re.compile(r'^.*?(?P<dt>[0-9]{4}-[0-9]{2}-[0-9]{2}T'
                                     r'[0-9]{2}:[0-9]{2}:[0-9]{2})_'
                                     r'tile(?P<tilepk>[0-9]{7})_'
                                     r'field(?P<fieldid>[0-9]{5})_'
