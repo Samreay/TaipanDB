7ef7996de0e01cf40231848029ab72d3ba92578b
= <marc.white@anu.edu.au>
2018-01-05 13:40:28 +1100

    Fix type-casting issues for new science load

--- taipandb/resources/v0_0_1/ingest/loadScience.py
+++ taipandb/resources/v0_0_1/ingest/loadScience.py
@@ -290,10 +290,10 @@ def execute(cursor, science_file=None, mark_active=True):
                     "ANCILLARY_FLAGS", "ANCILLARY_PRIORITY"]
     elif science_file.split('/')[-1] in [
         'wsu_targetCatalog.fits']:
-        values_table1 = [[row['target_id'],
+        values_table1 = [[int(row['target_id']),
                           float(row['ra']), float(row['dec']),
                           True, False, False, False,
-                          ] + list(polar2cart((float(row['ra']), float(row['dec'])))) for
+                          ] + list(polar2cart((row['ra'], row['dec']))) for
                          row in science_table]
         columns1 = ["TARGET_ID", "RA", "DEC", "IS_SCIENCE", "IS_STANDARD",
                     "IS_GUIDE", "IS_SKY", "UX", "UY", "UZ"]
