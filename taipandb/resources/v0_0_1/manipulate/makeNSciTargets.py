f48d73f1120f53ed90192f5ee068b4564ad21f5f
= <marc.white@anu.edu.au>
2018-01-12 10:14:24 +1100

    Added done target count logging

--- taipandb/resources/v0_0_1/manipulate/makeNSciTargets.py
+++ taipandb/resources/v0_0_1/manipulate/makeNSciTargets.py
@@ -187,6 +187,8 @@ def multithread_task(fields,
                                                       ('field_id', 'IN',
                                                        fields),
                                                   ])
+    logging.debug('Done target counts:')
+    logging.debug(done_target_count)
     done_target_count = [[_['field_id'], _['count']] for _ in done_target_count]
     update_rows(cursor_internal, 'tiling_info', done_target_count,
                 columns=['field_id', 'n_done'],
